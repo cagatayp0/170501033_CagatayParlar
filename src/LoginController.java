b384fc224451d64ac72d6369811b9231c0180eb3
cagatay <cagatayparlar99@gmail.com>
2022-06-25 20:20:48 +0300

    new login scene and controller

---
+++ src/LoginController.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class LoginController implements Initializable  {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+    public static void closeProgram(){
+        
+    }
+    
+    public static void switchToAdminLog() {
+        
+    }
+    
+    public static void switchToProfLog() {
+        
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class OzDeKurFX extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
